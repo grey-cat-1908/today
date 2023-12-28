442408edb195c6ed90399f34d5f66f1ddd0348bf
Виктор <61203964+grey-cat-1908@users.noreply.github.com>
2023-12-28 10:31:54 +0300

    maybe

--- constants.js
+++ constants.js
@@ -6,5 +6,5 @@ const mid = Math.floor(midnightUTC.getTime() / 10000);
 
 const A = 3, B = 0.7, C = 5, D = 13, E = 11, F = 17, G = 2, H = 1, I = 23;
 const T = 86400;
-const typeRange = [87, 79, 767, 243];
-const typeStart = [128640, 128512, 127744, 129292];
+const typeRange = [87, 79, 240, 243, 317];
+const typeStart = [128640, 128512, 127744, 129292, 128000];
